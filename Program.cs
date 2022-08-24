89e379ca6b53fa3676bf89bc414a08af273944f9
kotableOrKisaki <super_misha05@mail.ru>
2022-08-24 20:29:15 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace seventhTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesInHour = 60;
+            int visitTimePerPerson = 10;
+            int peopleCountInLine;
+            int totalWaitingTime;
+            int waitingHoursInLine;
+            int waitingMinutesInLine;
+
+            Console.Write("Сколько людей перед вами в очереди?\t");
+            peopleCountInLine = Convert.ToInt32(Console.ReadLine());
+            totalWaitingTime = visitTimePerPerson * peopleCountInLine;
+            waitingHoursInLine = totalWaitingTime / minutesInHour;
+            waitingMinutesInLine = totalWaitingTime % minutesInHour;
+
+            Console.Write("Вам предстоит подождать {0} час(а) и {1} минут(ы)\n", waitingHoursInLine, waitingMinutesInLine);
+        }
+    }
+}
